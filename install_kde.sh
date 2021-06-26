32b06f60c91aced0fa0d049996c51ab77556869b
Ming Ju <VincentUCLA@users.noreply.github.com>
2021-06-26 16:27:52 -0400

    Update install_kde.sh

--- install_kde.sh
+++ install_kde.sh
@@ -6,3 +6,5 @@ echo "exec /usr/bin/startkde" >> ~/.xinitrc
 dnf install -y sddm firefox
 systemctl enable sddm
 systemctl set-default graphical.target
+dnf groupremove 'X Window System' 'GNOME'
+dnf groupremove "Graphical Administration Tools" --skip-broken --nobest
